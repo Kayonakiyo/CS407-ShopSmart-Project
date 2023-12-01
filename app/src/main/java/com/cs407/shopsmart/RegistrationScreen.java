f7fa492fda8208c79312f9d8259a3be3c3e35c77
Kayley Seow <kayleyseow8@gmail.com>
2023-12-01 15:54:44 -0600

    Update the UI for Login and Register.

--- app/src/main/java/com/cs407/shopsmart/RegistrationScreen.java
+++ app/src/main/java/com/cs407/shopsmart/RegistrationScreen.java
@@ -1,8 +1,11 @@
 package com.cs407.shopsmart;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class RegistrationScreen extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class RegistrationScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen);
+
+        // Shows the nav arrow on top of screen
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * Deals with navigating back by one activity.
+     *
+     * @param item The menu item that was selected.
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == android.R.id.home){
+            navigateUpTo(new Intent(this, HomeLoginScreen.class));
+            return true;
+        }
+        return false;
     }
 }
