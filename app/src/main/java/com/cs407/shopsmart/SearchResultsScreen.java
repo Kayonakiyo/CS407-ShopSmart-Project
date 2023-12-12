b59d3a7f681af2df92d4b12125801f772d5c9e05
elaineli <elaineli7243@gmail.com>
2023-12-11 20:18:36 -0600

    added connection to serverside

--- app/src/main/java/com/cs407/shopsmart/SearchResultsScreen.java
+++ app/src/main/java/com/cs407/shopsmart/SearchResultsScreen.java
@@ -10,12 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class SearchResultsScreen extends AppCompatActivity {
 
@@ -33,7 +40,7 @@ public class SearchResultsScreen extends AppCompatActivity {
         searchEditText = findViewById(R.id.searchItem);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        allItems = loadItemsFromJson(); // Load the data from JSON
+        //allItems = loadItemsFromJson(); // Load the data from JSON
         adapter = new SearchResultsAdapter(new ArrayList<>()); // Initialize with an empty list
         recyclerView.setAdapter(adapter);
 
@@ -51,7 +58,29 @@ public class SearchResultsScreen extends AppCompatActivity {
 
     private void performSearch(String query) {
         List<ShoppingCartData> filteredList = new ArrayList<>();
-        for (ShoppingCartData item : allItems) {
+        ArrayList<ShoppingCartData> queriedData = new ArrayList<>();
+        try{
+            URL url = new URL("http://api.resolyth.dev");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type","application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            String payload = "{\"query\":\"" + query + "\"}";// This should be your json body i.e. {"Name" : "Mohsin"}
+            byte[] out = payload.getBytes(StandardCharsets.UTF_8);
+            OutputStream stream = connection.getOutputStream();
+            stream.write(out);
+            String s = new String(out, StandardCharsets.UTF_8);
+            queriedData = (ArrayList<ShoppingCartData>) loadItemsFromJson(s);
+            System.out.println(connection.getResponseCode() + " " + connection.getResponseMessage()); // THis is optional
+            connection.disconnect();
+        }catch (Exception e){
+            System.out.println(e);
+            System.out.println("Failed successfully");
+        }
+
+
+        for (ShoppingCartData item : queriedData) {
             if (item.getName().toLowerCase().contains(query.toLowerCase())) {
                 filteredList.add(item);
             }
@@ -59,13 +88,13 @@ public class SearchResultsScreen extends AppCompatActivity {
         adapter.updateList(filteredList);
     }
 
-    private List<ShoppingCartData> loadItemsFromJson() {
+    private List<ShoppingCartData> loadItemsFromJson(String rawData) {
         Gson gson = new Gson();
         Type itemListType = new TypeToken<ArrayList<ShoppingCartData>>(){}.getType();
         try {
-            InputStreamReader isr = new InputStreamReader(getAssets().open("UWBSData.json"));
-            return gson.fromJson(isr, itemListType);
-        } catch (IOException e) {
+            Scanner scnr = new Scanner(rawData);
+            return gson.fromJson(rawData, itemListType);
+        } catch (JsonSyntaxException e) {
             Toast.makeText(this, "Error loading JSON data: " + e.getMessage(), Toast.LENGTH_LONG).show();
             return new ArrayList<>(); // Return an empty list if there's an error
         }
